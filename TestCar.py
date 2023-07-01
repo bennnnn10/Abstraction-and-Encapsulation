cf6ecdb95a5de46afc5d4b63e16ecad782e4cdd4
unknown <rv8828120@gmail.com>
2023-07-02 00:30:28 +0800

    Accelerates 5 times

--- TestCar.py
+++ TestCar.py
@@ -4,4 +4,29 @@ from Car import Car
 car_info = Car(2022, "Honda Civic Type R")
 
 # Accelerates 5 times
+for _ in range (1):
+    car_info.accelerate()
+    speed = car_info.get_speed()
+    print("The car's speed right now is", speed)
+
+for _ in range (1):
+    car_info.accelerate()
+    speed = car_info.get_speed()
+    print("Accelerating to", speed)
+
+for _ in range (1):
+    car_info.accelerate()
+    speed = car_info.get_speed()
+    print("Accelerating to", speed)
+
+for _ in range (1):
+    car_info.accelerate()
+    speed = car_info.get_speed()
+    print("Accelerating to", speed)
+
+for _ in range (1):
+    car_info.accelerate()
+    speed = car_info.get_speed()
+    print("The current speed is", speed)
+
 # Brakes 5 times
