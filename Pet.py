5eeee01f724088c1fa858ed102afdb2939cad1a6
unknown <rv8828120@gmail.com>
2023-07-02 12:28:15 +0800

    Class Pet and Pseudocode

--- Pet.py
+++ Pet.py
@@ -1,3 +1,12 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+class Pet:
+    # Constructor
+        # Private Members
+    # Setter Method
+    # Getter Method
+        # Getting the name
+        # Getting animal type
+        #Getting its age
