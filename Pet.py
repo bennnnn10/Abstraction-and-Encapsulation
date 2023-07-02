97a55464936c0eb71eeeba091f865e8ce3b94d83
unknown <rv8828120@gmail.com>
2023-07-02 12:43:19 +0800

    Getter Method

--- Pet.py
+++ Pet.py
@@ -9,9 +9,17 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
-        
+
     # Setter Method
     # Getter Method
+    def get_name(self):
         # Getting the name
+        return self.__name
+    
+    def get_animal_type(self):
         # Getting animal type
+        return self.__animal_type
+    
+    def get_age(self):
         #Getting its age
+        return self.__age
