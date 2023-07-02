a1fab860ba964ae73005adafcffcf6747f7facda
unknown <rv8828120@gmail.com>
2023-07-02 14:11:51 +0800

    Input informations

--- TestPet.py
+++ TestPet.py
@@ -1,3 +1,11 @@
 from Pet import Pet
 
 pet = Pet()
+
+pet_name = input("What is the name of your pet? ")
+animal_type = input("What type of animal is your pet? ")
+age = int(input("How old is you pet? "))
+
+pet.set_name(pet_name)
+pet.set_animal_type(animal_type)
+pet.set_age(age)
