99ffb5767b8e6260293e2db7aaa2573e318e8c23
unknown <rv8828120@gmail.com>
2023-07-02 17:26:02 +0800

    Class Fan and its three constants

--- Fan.py
+++ Fan.py
@@ -1,3 +1,9 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+class Fan:
+    # Three constants named SLOW, MEDIUM, and FAST with the values 1,2, and 3.
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
