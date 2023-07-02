8d58db9bba3a1c2428b2c1000c98e82289af42b7
unknown <rv8828120@gmail.com>
2023-07-02 17:28:59 +0800

    Pseudocode

--- Fan.py
+++ Fan.py
@@ -7,3 +7,13 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # Constructor
+        # Private Methods
+        # Private int data field named speed
+        # Private bool data field named on that specifies whether the fan is on
+        # Private float data field named radius
+        # Private string data field named color
+
+    # Getter Method
+    # Setter Method
